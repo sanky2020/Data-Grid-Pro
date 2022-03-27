3a82ac50fe352b30cc756545c4592c57f42adc98
sanky2020 <sankyk2020@gmail.com>
2022-03-27 19:11:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DatagridproComponent from './Component/DatagridproComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <DatagridproComponent/>
+    </div>
+  );
+}
+
+export default App;
